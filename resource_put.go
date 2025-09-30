246275583b5f8ddb2d06c565054e8b6a181d3c03
sukasukasuka123 <redred55@qq.com>
2025-09-30 11:52:24 +0800

    Update resource_put.go
    no mutex

--- resource_put.go
+++ resource_put.go
@@ -3,20 +3,32 @@ package pool
 import (
 	"context"
 	"sync/atomic"
+	"time"
 )
 
-func (p *Pool[T]) PutResource(ctx context.Context, resource *resource[T]) error {
-	go p.resetResource(resource, ctx)
-	return nil
+func (p *Pool[T]) PutResource(ctx context.Context, res *resource[T]) error {
+	// 更新资源时间
+	res.updateTime = time.Now()
+	var resCh = p.Resources.Load()
+	// 尝试非阻塞放回池子
+	select {
+	case *resCh <- res:
+		return nil
+	default:
+		// 池满，尝试触发扩容
+		select {
+		case p.expandChan <- struct{}{}:
+		default:
+		}
+		// 关闭资源并减计数
+		return p.closeResource(res)
+	}
 }
 func (p *Pool[T]) closeResource(resource *resource[T]) error {
-	p.ConnLock.Lock()
-	defer p.ConnLock.Unlock()
 	err := p.connControl.Close(resource.Conn)
 	if err != nil {
 		return err
 	}
-	// p.nowsize--
 	atomic.AddInt64(&p.nowsize, -1)
 	return nil
 }
